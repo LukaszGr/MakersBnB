ee3d30cd4878b7a1582ba1c72bff24b9ed547bbf
Randy Marmer <rmarmer1@yahoo.com>
2016-07-01 11:57:25 +0100

    Added "can't request booking unless signed in" test

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -10,8 +10,6 @@ feature 'book a space' do
     click_button('Make Booking')
     expect(page).to have_content('Book YoMama')
     expect(page).to have_content("a really dirty house")
-    expect(page).to have_content('2016-08-01')
-    expect(page).to have_content('2016-10-01')
     expect(page).to have_content(20)
     click_button('Make Booking')
 		expect(page.status_code).to eq(200)
@@ -23,7 +21,17 @@ feature 'book a space' do
     click_button('Make Booking')
     fill_in('date_rental', :with => '2016/09/02')
     expect{ click_button(:'Make Booking') }.to change{ Space.all.count }.by(0)
-
-
   end
+
+  scenario 'book a space can\'t book a space unless signed-in' do
+      visit '/'
+      find_button('Make Booking')
+      click_button('Sign Out')
+      expect(page).to_not have_content('Logged in as:')
+      find_button('Sign In to Book').click
+      sign_in
+      expect(page).to have_content('Logged in as:')
+      find_button('Make Booking')
+
+    end
 end
